a79b7b967cf42eed1c898b52127919fc05b436c5
RuiBorda <bordarui@gmail.com>
2025-06-02 22:09:23 -0500

    fix: tag builder description in english

--- src/swagger/TagBuilder.go
+++ src/swagger/TagBuilder.go
@@ -1,8 +1,8 @@
 package swagger
 
 import (
-	"github.com/ruiborda/go-swagger-generator/src/openapi"
-	entity2 "github.com/ruiborda/go-swagger-generator/src/openapi_spec"
+	"github.com/ruiborda/go-swagger-generator/v2/src/openapi"
+	entity2 "github.com/ruiborda/go-swagger-generator/v2/src/openapi_spec"
 )
 
 type TagBuilder struct {
