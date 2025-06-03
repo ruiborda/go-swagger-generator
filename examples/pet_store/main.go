e8343102dd69d10514fac416fd990c76909991f5
RuiBorda <bordarui@gmail.com>
2025-06-02 22:06:39 -0500

    fix: update pet store main example

--- examples/pet_store/main.go
+++ examples/pet_store/main.go
@@ -4,9 +4,9 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/ruiborda/go-swagger-generator/examples/pet_store/controller"
-	"github.com/ruiborda/go-swagger-generator/src/middleware"
-	"github.com/ruiborda/go-swagger-generator/src/openapi"
-	"github.com/ruiborda/go-swagger-generator/src/swagger"
+	"github.com/ruiborda/go-swagger-generator/v2/src/middleware"
+	"github.com/ruiborda/go-swagger-generator/v2/src/openapi"
+	"github.com/ruiborda/go-swagger-generator/v2/src/swagger"
 )
 
 func main() {
