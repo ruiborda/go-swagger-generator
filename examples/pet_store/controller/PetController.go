c1adf3da1a919d031a44b14fb68eac22b84cea62
RuiBorda <bordarui@gmail.com>
2025-06-02 22:06:39 -0500

    fix: update PetController logic

--- examples/pet_store/controller/PetController.go
+++ examples/pet_store/controller/PetController.go
@@ -5,9 +5,9 @@ import (
 	"strconv"
 
 	"github.com/gin-gonic/gin"
-	"github.com/ruiborda/go-swagger-generator/src/openapi"
-	"github.com/ruiborda/go-swagger-generator/src/openapi_spec/mime"
-	"github.com/ruiborda/go-swagger-generator/src/swagger"
+	"github.com/ruiborda/go-swagger-generator/v2/src/openapi"
+	"github.com/ruiborda/go-swagger-generator/v2/src/openapi_spec/mime"
+	"github.com/ruiborda/go-swagger-generator/v2/src/swagger"
 )
 
 // Pet DTOs
