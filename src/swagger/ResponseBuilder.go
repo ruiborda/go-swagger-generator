9f5bd665a917b44b649106d6b64df6b68fb07fbc
RuiBorda <bordarui@gmail.com>
2025-06-02 22:09:23 -0500

    fix: response builder description in english

--- src/swagger/ResponseBuilder.go
+++ src/swagger/ResponseBuilder.go
@@ -1,9 +1,9 @@
 package swagger
 
 import (
-	"github.com/ruiborda/go-swagger-generator/src/openapi"
-	entity "github.com/ruiborda/go-swagger-generator/src/openapi_spec"
-	"github.com/ruiborda/go-swagger-generator/src/openapi_spec/mime"
+	"github.com/ruiborda/go-swagger-generator/v2/src/openapi"
+	entity "github.com/ruiborda/go-swagger-generator/v2/src/openapi_spec"
+	"github.com/ruiborda/go-swagger-generator/v2/src/openapi_spec/mime"
 )
 
 type ResponseBuilder struct {
