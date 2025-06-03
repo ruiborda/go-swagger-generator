066a7de74d7b39fa2bd4166ad3fb6f9e56c9a3b5
RuiBorda <bordarui@gmail.com>
2025-06-02 22:06:39 -0500

    fix: update basic example

--- examples/basic/main.go
+++ examples/basic/main.go
@@ -3,10 +3,9 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"github.com/ruiborda/go-swagger-generator/src/middleware"
-	"github.com/ruiborda/go-swagger-generator/src/openapi"
-	"github.com/ruiborda/go-swagger-generator/src/openapi_spec/mime"
-	"github.com/ruiborda/go-swagger-generator/src/swagger"
+	"github.com/ruiborda/go-swagger-generator/v2/src/openapi"
+	"github.com/ruiborda/go-swagger-generator/v2/src/openapi_spec/mime"
+	"github.com/ruiborda/go-swagger-generator/v2/src/swagger"
 	"net/http"
 )
 
@@ -18,10 +17,11 @@ type UserDto struct {
 func main() {
 	router := gin.Default()
 
-	// This will add UserDto to components.schemas
-	_, _ = swagger.Swagger().SchemaFromDTO(&UserDto{})
+	// Registrar el esquema UserDto en Swagger
+	doc := swagger.Swagger()
+	_, _ = doc.SchemaFromDTO(&UserDto{})
 
-	ConfigureSwagger(router)
+	ConfigureSwagger(doc)
 
 	router.GET("/v1/users/:id", GetUserById)
 
@@ -31,28 +31,38 @@ func main() {
 	_ = router.Run(":8080")
 }
 
-// Note: Path in doc.Path() should NOT include the base path from servers configuration.
-// The server URL will be prepended by UI tools.
-var _ = swagger.Swagger().Path("/users/{id}"). // Path relative to server URL
-						Get(func(op openapi.Operation) {
-		op.Summary("Find user by ID").
-			Tag("UserController"). // Tags are for grouping, can be any string
-			OperationID("getUserById").
-			PathParameter("id", func(p openapi.Parameter) {
-				p.Description("ID of the user to retrieve").
-					Required(true).
-					Schema(func(s openapi.Schema) {
-						s.Type("integer").Format("int64")
-					})
-			}).
-			Response(http.StatusOK, func(r openapi.Response) {
-				r.Description("successful operation").
-					Content(mime.ApplicationJSON, func(mt openapi.MediaType) {
-						mt.SchemaFromDTO(&UserDto{}) // Reference the DTO
-					})
-			})
+// Documentación de la ruta usando la API v2
+func ConfigureSwagger(doc openapi.SwaggerDocBuilder) {
+	doc.Info(func(info openapi.Info) {
+		info.Title("Simple API with OAS3").
+			Version("1.0").
+			Description("This is a simple API example using Swagger and OpenAPI 3.0.")
 	}).
-	Doc()
+		Server("http://localhost:8080/v1", func(server openapi.Server) {
+			server.Description("Local development server V1")
+		})
+
+	doc.Path("/users/{id}", func(path openapi.PathItem) {
+		path.Get(func(op openapi.Operation) {
+			op.Summary("Find user by ID").
+				Tag("UserController").
+				OperationID("getUserById").
+				PathParameter("id", func(p openapi.Parameter) {
+					p.Description("ID of the user to retrieve").
+						Required(true).
+						Schema(func(s openapi.Schema) {
+							s.Type("integer").Format("int64")
+						})
+				}).
+				Response(http.StatusOK, func(r openapi.Response) {
+					r.Description("successful operation").
+						Content(mime.ApplicationJSON, func(mt openapi.MediaType) {
+							mt.SchemaFromDTO(&UserDto{})
+						})
+				})
+		})
+	})
+}
 
 func GetUserById(c *gin.Context) {
 	id := c.Param("id")
@@ -60,25 +70,4 @@ func GetUserById(c *gin.Context) {
 		ID:   1, // Example ID, could parse from 'id' param
 		Name: "John Doe for ID " + id,
 	})
-}
-
-func ConfigureSwagger(router *gin.Engine) {
-	router.Use(middleware.SwaggerGin(middleware.SwaggerConfig{
-		Enabled:  true,
-		JSONPath: "/openapi.json",
-		UIPath:   "/",
-		Title:    "Simple API OAS3",
-	}))
-
-	doc := swagger.Swagger()
-
-	doc.Info(func(info openapi.Info) {
-		info.Title("Simple API with OAS3").
-			Version("1.0").
-			Description("This is a simple API example using Swagger and OpenAPI 3.0.")
-	}).
-		Server("http://localhost:8080/v1", func(server openapi.Server) { // Server URL now includes base path
-			server.Description("Local development server V1")
-		})
-	// No BasePath() or Schemes() in OAS3 at the root level, handled by Servers array.
 }
