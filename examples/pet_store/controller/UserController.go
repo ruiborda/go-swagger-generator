92bd3ff22bf84d68cd0148732ecb918e814924f9
RuiBorda <bordarui@gmail.com>
2025-06-02 22:06:39 -0500

    fix: update UserController logic

--- examples/pet_store/controller/UserController.go
+++ examples/pet_store/controller/UserController.go
@@ -4,9 +4,9 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/ruiborda/go-swagger-generator/src/openapi"
-	"github.com/ruiborda/go-swagger-generator/src/openapi_spec/mime"
-	"github.com/ruiborda/go-swagger-generator/src/swagger"
+	"github.com/ruiborda/go-swagger-generator/v2/src/openapi"
+	"github.com/ruiborda/go-swagger-generator/v2/src/openapi_spec/mime"
+	"github.com/ruiborda/go-swagger-generator/v2/src/swagger"
 )
 
 // User DTO
